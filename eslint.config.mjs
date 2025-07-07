fb78597f49b639fea5f7389737430b2479a45385
HM-Webcoding <hamidmonju1@gmail.com>
2025-07-07 21:58:04 +0600

    updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,14 +13,12 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      // Disable unused variable errors
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": "off",
-
-      // Disable type-related rules
-      "@typescript-eslint/ban-types": "off", // Allow {} and other banned types
-      "@typescript-eslint/no-explicit-any": "off", // Allow use of 'any' type
-      "@typescript-eslint/explicit-module-boundary-types": "off", // Don't require return types
+      "@typescript-eslint/ban-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-non-null-assertion": "off", // allow `el!`
     },
   },
 ];
